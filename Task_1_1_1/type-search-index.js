8e89c8c497c6f5e2b421471e6b61b83b652ec57c
NikRo12 <NikRo12@users.noreply.github.com>
2025-09-13 12:53:34 +0000

    Deploying to gh-pages from @ NikRo12/OOP@5a172a2ea374605d81b5e42b0ca541c6dac77dcc 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanenko","l":"Sample"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanenko","l":"HeapSort"},{"p":"ru.nsu.romanenko","l":"Sample"}];updateSearchResults();
