f691d418d5580e4098d0d4f9c317f7c75426b04a
Nikita <nr.081105@gmail.com>
2025-09-28 14:46:14 +0700

    After reviw 1

--- Task_1_1_2/src/test/java/ru/nsu/romanenko/controllerTest/ControllerTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/romanenko/controllerTest/ControllerTest.java
@@ -39,9 +39,82 @@ class ControllerTest {
         Card hiddenCard = (Card) initRoundMethod.invoke(null, user, dealer);
 
         assertTrue(user.getPoints() > 0);
-
         assertTrue(dealer.getPoints() > 0);
-
         assertNotNull(hiddenCard);
+    }
+
+    @Test
+    void testInitRoundGivesDifferentCards() throws Exception {
+        Method initRoundMethod = Controller.class.getDeclaredMethod("initRound", PointsCounter.class, PointsCounter.class);
+        initRoundMethod.setAccessible(true);
+
+        PointsCounter user1 = new PointsCounter(false);
+        PointsCounter dealer1 = new PointsCounter(true);
+        PointsCounter user2 = new PointsCounter(false);
+        PointsCounter dealer2 = new PointsCounter(true);
+
+        Card hiddenCard1 = (Card) initRoundMethod.invoke(null, user1, dealer1);
+        Card hiddenCard2 = (Card) initRoundMethod.invoke(null, user2, dealer2);
+
+        assertNotEquals(user1.getPoints(), user2.getPoints());
+        assertNotEquals(dealer1.getPoints(), dealer2.getPoints());
+    }
+
+    @Test
+    void testUserGetsTwoCardsDealerGetsOne() throws Exception {
+        Method initRoundMethod = Controller.class.getDeclaredMethod("initRound", PointsCounter.class, PointsCounter.class);
+        initRoundMethod.setAccessible(true);
+
+        PointsCounter user = new PointsCounter(false);
+        PointsCounter dealer = new PointsCounter(true);
+
+        Card hiddenCard = (Card) initRoundMethod.invoke(null, user, dealer);
+
+        assertTrue(user.getPoints() > dealer.getPoints());
+    }
+
+    @Test
+    void testHiddenCardIsValid() throws Exception {
+        Method initRoundMethod = Controller.class.getDeclaredMethod("initRound", PointsCounter.class, PointsCounter.class);
+        initRoundMethod.setAccessible(true);
+
+        PointsCounter user = new PointsCounter(false);
+        PointsCounter dealer = new PointsCounter(true);
+
+        Card hiddenCard = (Card) initRoundMethod.invoke(null, user, dealer);
+
+        assertNotNull(hiddenCard.getSuit());
+        assertNotNull(hiddenCard.getValue());
+        assertTrue(hiddenCard.getNumericalValue() >= 2 && hiddenCard.getNumericalValue() <= 11);
+    }
+
+    @Test
+    void testMultipleInitRoundsWork() throws Exception {
+        Method initRoundMethod = Controller.class.getDeclaredMethod("initRound", PointsCounter.class, PointsCounter.class);
+        initRoundMethod.setAccessible(true);
+
+        for (int i = 0; i < 5; i++) {
+            PointsCounter user = new PointsCounter(false);
+            PointsCounter dealer = new PointsCounter(true);
+            Card hiddenCard = (Card) initRoundMethod.invoke(null, user, dealer);
+
+            assertTrue(user.getPoints() > 0);
+            assertTrue(dealer.getPoints() > 0);
+            assertNotNull(hiddenCard);
+        }
+    }
+
+    @Test
+    void testUserAndDealerHaveReasonablePoints() throws Exception {
+        Method initRoundMethod = Controller.class.getDeclaredMethod("initRound", PointsCounter.class, PointsCounter.class);
+        initRoundMethod.setAccessible(true);
+
+        PointsCounter user = new PointsCounter(false);
+        PointsCounter dealer = new PointsCounter(true);
+
+        Card hiddenCard = (Card) initRoundMethod.invoke(null, user, dealer);
+
+        assertTrue(user.getPoints() >= 4 && user.getPoints() <= 21);
+        assertTrue(dealer.getPoints() >= 2 && dealer.getPoints() <= 11);
     }
 }
