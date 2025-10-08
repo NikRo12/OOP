f2a3061cf1d285539926718c22d4b4060fb1f054
NikRo12 <NikRo12@users.noreply.github.com>
2025-10-08 11:16:32 +0000

    Deploying to gh-pages from @ NikRo12/OOP@fba9bd4997831788d75119dab14d5895a9bf0502 🚀

---
+++ Task_1_2_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanenko","l":"HashTable.Entry"},{"p":"ru.nsu.romanenko","l":"HashTable"},{"p":"ru.nsu.romanenko","l":"Main"}];updateSearchResults();
