4f4041587cac63e88141a9961141a823448ea867
NikRo12 <NikRo12@users.noreply.github.com>
2025-10-14 07:05:10 +0000

    Deploying to gh-pages from @ NikRo12/OOP@28ba0c3deb2b7b21ed3cea24891e2c40941b2676 🚀

---
+++ Task_1_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.romanenko","l":"Main"},{"p":"ru.nsu.romanenko","l":"SubstringFinder"}];updateSearchResults();
