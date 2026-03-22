2a2e6a52875e76063c5f90c29a9f109afa5e986e
Nikita <nr.081105@gmail.com>
2026-03-22 23:17:56 +0700

    Task_2_3_1

--- Task_2_3_1/src/test/java/ru/nsu/romanenko/controller/GameControllerTest.java
+++ Task_2_3_1/src/test/java/ru/nsu/romanenko/controller/GameControllerTest.java
@@ -1,6 +1,5 @@
 package ru.nsu.romanenko.controller;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.nsu.romanenko.model.Direction;
@@ -13,14 +12,6 @@ public class GameControllerTest {
 
     private GameConfig config;
 
-    @BeforeAll
-    static void initJavaFX() {
-        try {
-            javafx.application.Platform.startup(() -> {});
-        } catch (IllegalStateException ignored) {
-        }
-    }
-
     @BeforeEach
     void setUp() {
         config = new GameConfig(10, 10, 2, 0.0, 5, 200, 30);
@@ -29,78 +20,32 @@ public class GameControllerTest {
     @Test
     void initialStateIsWaiting() {
         GameController controller = new GameController(config);
-        controller.start();
         assertEquals(GameState.WAITING, controller.getState());
-    }
-
-    @Test
-    void handleSpaceFromWaitingTransitionsToRunning() {
-        GameController controller = new GameController(config);
-        controller.start();
-        controller.handleSpace();
-        assertEquals(GameState.RUNNING, controller.getState());
-    }
-
-    @Test
-    void handleSpaceWhileRunningDoesNothing() {
-        GameController controller = new GameController(config);
-        controller.start();
-        controller.handleSpace();
-        controller.handleSpace();
-        assertEquals(GameState.RUNNING, controller.getState());
     }
 
     @Test
     void handleInputIgnoredWhenWaiting() {
         GameController controller = new GameController(config);
-        controller.start();
         assertDoesNotThrow(() -> controller.handleInput(Direction.LEFT));
         assertEquals(GameState.WAITING, controller.getState());
     }
 
     @Test
-    void handleInputIgnoredWhenLost() {
+    void fieldIsNotNullAfterConstruction() {
         GameController controller = new GameController(config);
-        controller.start();
-        controller.handleSpace();
-        controller.stop();
-        assertDoesNotThrow(() -> controller.handleInput(Direction.UP));
-    }
-
-    @Test
-    void handleSpaceAfterLostResetsToWaiting() {
-        GameController controller = new GameController(config);
-        controller.start();
-        controller.handleSpace();
-        controller.stop();
-
-        GameConfig tinyWin = new GameConfig(10, 10, 1, 0.0, 1, 200, 30);
-        GameController c2 = new GameController(tinyWin);
-        c2.start();
-        assertEquals(GameState.WAITING, c2.getState());
-        c2.handleSpace();
-        assertEquals(GameState.RUNNING, c2.getState());
-    }
-
-    @Test
-    void fieldIsNotNullAfterStart() {
-        GameController controller = new GameController(config);
-        controller.start();
         assertNotNull(controller.getField());
     }
 
     @Test
     void fieldHasCorrectDimensionsFromConfig() {
         GameController controller = new GameController(config);
-        controller.start();
         assertEquals(10, controller.getField().getHorizontalSize());
         assertEquals(10, controller.getField().getVerticalSize());
     }
 
     @Test
-    void stopDoesNotThrowWhenTimelineExists() {
+    void stopDoesNotThrowBeforeStart() {
         GameController controller = new GameController(config);
-        controller.start();
         assertDoesNotThrow(controller::stop);
     }
 }
